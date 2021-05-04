6f852d1926bd4ae22fdfb0ceb52c49f5617e1e86
Xi Hong <40461799+harryhongxi@users.noreply.github.com>
2021-05-05 01:14:09 +0800

    Add files via upload

---
+++ ila_2/ila_2_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+// Date        : Thu Feb  4 16:50:00 2021
+// Host        : Harry-PC running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub {g:/Liu
+//               Task/ddr3_v3_1/ddr3/s7_ddr3_testMINE/Project/ddr3_test.srcs/sources_1/ip/ila_2/ila_2_stub.v}
+// Design      : ila_2
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tftg256-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "ila,Vivado 2017.4" *)
+module ila_2(clk, probe0, probe1, probe2, probe3, probe4, probe5)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe0[0:0],probe1[0:0],probe2[0:0],probe3[0:0],probe4[0:0],probe5[11:0]" */;
+  input clk;
+  input [0:0]probe0;
+  input [0:0]probe1;
+  input [0:0]probe2;
+  input [0:0]probe3;
+  input [0:0]probe4;
+  input [11:0]probe5;
+endmodule
